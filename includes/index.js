2d47bfe160a28fb89b5506c27014d01a5a65e2d5
Khor Ze Hung <84367877+KhorZeHung@users.noreply.github.com>
2023-03-21 15:55:40 +0800

    edit post

--- includes/index.js
+++ includes/index.js
@@ -1,43 +1,64 @@
-function toggleNav(x) {
-  if (document.getElementById("mySidenav").style.width === "200px") {
-    closeNav(x);
-  } else {
-    openNav(x);
-  }
-}
+$(window).scroll(function () {
+  var value = $(this).scrollTop() > 200 ? "block" : "none";
+  $("#scrollTopBtn").css("display", value);
+});
 
-function closeNav(x) {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementsByClassName(x)[0].classList.remove("change");
-}
+$("#scrollTopBtn").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 100);
+});
 
-function openNav(x) {
-  document.getElementById("mySidenav").style.width = "200px";
-  document.getElementsByClassName(x)[0].classList.add("change");
-}
+$(".headerNav").click(function () {
+  $(".headerNav").toggleClass("change");
+  $("#mySidenav").toggleClass("openSideNav");
+});
 
-window.onclick = function (event) {
+$(document).click(function (event) {
   if (
-    document.getElementById("mySidenav").style.width === "200px" &&
-    !document.getElementsByClassName("headerNav")[0].contains(event.target)
+    $("#mySidenav").css("width") === "200px" &&
+    !$(".headerNav").is(event.target)
   ) {
-    closeNav("headerNav");
+    $(".headerNav").removeClass("change");
+    $("#mySidenav").removeClass("openSideNav");
   }
-};
-//Show the modal if parameter mention
+});
 
-window.onload = () => {
+$(document).ready(function () {
   var searchParams = new URLSearchParams(window.location.search);
   if (searchParams.has("login") && searchParams.get("login") == 1) {
-    var modal = document.getElementById("loginForm");
-    modal.style.display = "block";
-  }else if(searchParams.has("addAlbum") && searchParams.get("addAlbum") == 1){
-    var modal = document.getElementById("addAlbum");
-    modal.style.display = "block";
+    $("#loginForm").css("display", "block");
+  } else if (
+    searchParams.has("addAlbum") &&
+    searchParams.get("addAlbum") == 1
+  ) {
+    $("#addAlbum").css("display", "block");
+  }
+});
+
+var toggleDropDown = (ele) => {
+  var dropDownMenu = document.getElementById(ele);
+  dropDownMenu.classList.toggle("show");
+};
+
+var updateAlbumRadio = (toggleEle, value) => {
+  toggleDropDown(toggleEle);
+  if (document.getElementById("albumValue").textContent === "No Album Chosen") {
+    document.getElementById("albumValue").innerText = value;
+  } else {
+    document.getElementById("albumValue").innerText = "No Album Chosen";
   }
 };
 
-var toggleDropDown = () => {
-  var dropDownMenu = document.getElementById("albumDropDown");
-  dropDownMenu.classList.toggle("show");
-};
+$("#searchBar").focusin(function () {
+  $("#searchResult").show();
+  if ($(window).width() <= 600) {
+    $("#logo").hide();
+  }
+});
+
+$("#searchBar").focusout(function () {
+  $("#searchResult").hide();
+  $(this).val("");
+  setTimeout(() => {
+    $("#logo").show();
+  }, 300);
+});
